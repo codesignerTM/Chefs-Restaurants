0cb642adb879d5f2caf5a4cc515d9a4100f5a0dd
Tamás Mező <codesigner.hu@gmail.com>
2020-04-12 22:09:40 +0200

    building models and adding to db connection

--- services/src/db/connection.ts
+++ services/src/db/connection.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 
 import accessEnv from "#root/helpers/accessEnv";
+import models from "./models";
 
 const DB_URL = accessEnv("DB_URL");
 
@@ -10,7 +11,7 @@ const sequelize = new Sequelize(DB_URL, {
     multipleStatements: true,
   },
   logging: false,
-  //models
+  models,
 });
 
 export default sequelize;
