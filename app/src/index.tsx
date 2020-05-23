77af38a82a7e04370bfaf448c7cd76c1b44ff974
Tamás Mező <tamas.mezo@zooshgroup.com>
2020-05-23 22:47:21 +0200

    setting up apollo & global style

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,26 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
+import { ApolloProvider } from "react-apollo";
 
-render(<h1>Working</h1>, document.getElementById("app"))
+import graphqlClient from "./api/graphql";
+import { Root } from "./components/Root";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap');
+
+    body {
+        font-family: Roboto, sans-serfif;
+    }
+    `;
+
+
+
+
+render(
+    <ApolloProvider client={graphqlClient}>
+        <GlobalStyle />
+        <Root />
+    </ApolloProvider>,
+    document.getElementById("app")
+);
